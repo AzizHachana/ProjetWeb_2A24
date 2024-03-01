0705e8c167f60011f28a47872213afa87dce262f
AzizHachana <mohamedaziz.hachana@esprit.tn>
2024-03-01 02:09:16 +0100

    S6

---
+++ ex1.js
@@ -0,0 +1,8 @@
+const T = [17, 38, 10, 25, 72];
+T.sort() ;
+console.log(T) ;
+T.push(12) ; console.log(T) ;
+T.reverse() ;
+T.indexOf(17) ;
+T.splice(T.indexOf(38), 1)
+console.log(T) ;;
---
+++ index2.js
@@ -0,0 +1,37 @@
+function test(){
+    alert('Hello 2A24');return false ;
+                }
+console.log("ceci un test");
+let p=prompt("Saisir votre nome","votre nom");
+console.log(typeof(p));
+
+let c=confirm("voulez vous quitter la salle");
+console.log(typeof(c));
+var a=5;
+console.log("la valeur de a est" +a);
+console.log("la valeur de b est" +b);
+var b
+let x,y;
+console.log("la valeur de x est"+x);
+
+console.log("la valeur de y est"+y);
+
+if (true){
+    var z=2020;
+}
+console.log(z);
+if (true){
+    let y='hello';
+}
+console.log(y);
+let t="test";
+console.log(typeof(t));
+
+var arr1=[5];
+var arr2=Array(5);
+var arr3=Array(6);
+var arr4=Array.of(5);
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
